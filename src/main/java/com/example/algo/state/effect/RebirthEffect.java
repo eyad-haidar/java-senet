c0b570b6cf2f68926121eebd09403e69b179e63e
yousef1110ya <yousefalshikhali1711@gmail.com>
2026-01-09 20:20:25 +0300

    added a stick throw logic

--- src/main/java/com/example/algo/state/effect/RebirthEffect.java
+++ src/main/java/com/example/algo/state/effect/RebirthEffect.java
@@ -6,8 +6,6 @@ import com.example.algo.state.Piece;
 
 public class RebirthEffect implements CellEffect{
 	public void apply(Piece peice , GameState state) {
-		// in here we would apply the sudo code : 
-		// 1- return to the index 14 
-		// if index 14 had a piece then keep going down till you find it . 
+		// this will be a no-op , because when a player is on this peice it will have no effect . 
 	}
 }
--- src/main/java/com/example/algo/util/StickThrow.java
+++ src/main/java/com/example/algo/util/StickThrow.java
@@ -1,10 +1,17 @@
 package com.example.algo.util;
 
+import java.util.concurrent.ThreadLocalRandom;
 /*
  * DEV_NOTES: the returns in this class are wrong , just naming functions ,.
  */
 public class StickThrow {
-	public int throwSticks() {
-		return 0; 
+	public static int throwSticks() {
+		/*
+		 * this is a mathmatical representation , the bits value will be between 0->15 ( all will be represented with 4 bits ) 
+		 * and then the sum variable will be the count of 1's in the binary format of the randomly generated number . 
+		 * 		 */
+		int bits = ThreadLocalRandom.current().nextInt(16); 
+	    int sum = Integer.bitCount(bits);
+	    return sum == 0 ? 5 : sum;
 	}
 }
